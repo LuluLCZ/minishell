9b6e47a971ba219d5a872581c0aa5d3e2b65459b
Lucien LACAZE <llacaze@e2r1p11.42.fr>
2018-02-05 20:49:55 +0100

    ok

--- builtin.c
+++ builtin.c
@@ -6,7 +6,7 @@
 /*   By: llacaze <llacaze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/02 18:42:43 by llacaze           #+#    #+#             */
-/*   Updated: 2018/02/05 17:47:34 by llacaze          ###   ########.fr       */
+/*   Updated: 2018/02/05 20:49:30 by llacaze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,23 @@ char		*pwd_cwd(char *str)
 	return (new_str);
 }
 
+int			perm(char *path)
+{
+	struct stat		fstat;
+	int				i;
+
+	if (stat(path, &fstat) == -1)
+	{
+		// write(2, "cd: no such file or directory: ", 31);
+		// write(2, path, ft_strlen(path));
+		// write(2, "\n", 1);
+		i = -1;
+	}
+	else
+		i = ((fstat.st_mode & S_IRGRP) ? 1 : 0);
+	return (i);
+}
+
 void		builtin_cd(t_info *info)
 {
 	char	buf[512];
@@ -66,33 +83,89 @@ void		builtin_cd(t_info *info)
 	else if (ft_strcmp(info->line_tab[1], "-") == 0)
 	{
 		tmp = get_env(info->env, "OLDPWD");
-		dir = opendir(tmp);
-		if (dir != NULL)
+		if (perm(tmp) == 0)
 		{
-			info = get_env_num(info, "PWD", tmp);
-			info = get_env_num(info, "OLDPWD", buf);
-			chdir(tmp);
+			write(2, "cd: permission denied: ", 23);
+			write(2, tmp, ft_strlen(tmp));
+			write(2, "\n", 1);
 		}
 		else
 		{
-			write(2, "cd: no such file or directory: ", 31);
-			write(2, tmp, ft_strlen(tmp));
-			write(2, "\n", 1);
+			dir = opendir(tmp);
+			if (dir != NULL)
+			{
+				info = get_env_num(info, "PWD", tmp);
+				info = get_env_num(info, "OLDPWD", buf);
+				chdir(tmp);
+			}
+			else
+			{
+				write(2, "cd: no such file or directory: ", 31);
+				write(2, tmp, ft_strlen(tmp));
+				write(2, "\n", 1);
+			}
 		}
 	}
 	else if (dir == NULL)
 	{
-		write(2, "cd: no such file or directory: ", 31);
-		write(2, info->line_tab[1], ft_strlen(info->line_tab[1]));
-		write(2, "\n", 1);
+		tmp = (ft_strcmp(buf, "/") == 0) ? ft_strdup("/") : ft_strjoin(buf, "/");
+		if (perm(ft_strjoin(tmp, info->line_tab[1])) == 0)
+		{
+			write(2, "cd: permission denied: ", 23);
+			write(2, info->line_tab[1], ft_strlen(info->line_tab[1]));
+			write(2, "\n", 1);
+		}
+		else
+		{
+			write(2, "cd: no such file or directory: ", 31);
+			write(2, info->line_tab[1], ft_strlen(info->line_tab[1]));
+			write(2, "\n", 1);
+		}
 	}
 	else
 	{
-		tmp = (ft_strcmp(buf, "/") == 0) ? ft_strdup("/") : ft_strjoin(buf, "/");
-		info = get_env_num(info, "OLDPWD", buf);
-		chdir(ft_strjoin(tmp, info->line_tab[1]));
-		getcwd(buf, 512);
-		info = get_env_num(info, "PWD", buf);		
+		if ((dir = opendir(buf)) == NULL && info->line_tab[1][0] == '.' && info->line_tab[1][1] == '.')
+		{
+			int		i;
+			int		j;
+
+			j = 0;
+			i = 0;
+			if (info->line_tab[1][2] == '/')
+			{
+				tmp = get_env(info->env, "PWD");
+				while (tmp[i] != '\0')
+				{
+					if (j == ft_check_char(tmp, '/'))
+						break ;
+					if (tmp[i] == '/')
+						j++;
+					i++;
+				}
+				tmp[i] = '\0';
+				info->line_tab[1] = ft_strjoin(tmp, info->line_tab[1] + 3);
+				chdir(info->line_tab[1]);
+				info = get_env_num(info, "OLDPWD", info->line_tab[1]);
+				getcwd(buf, 512);
+				info = get_env_num(info, "PWD", buf);
+			}
+			else
+			{
+				chdir(get_env(info->env, "OLDPWD"));
+				getcwd(buf, 512);
+				info = get_env_num(info, "OLDPWD", get_env(info->env, "PWD"));
+				info = get_env_num(info, "PWD", buf);
+			}
+		}
+		else
+		{
+			tmp = (ft_strcmp(buf, "/") == 0) ? ft_strdup("/") : ft_strjoin(buf, "/");
+			info = get_env_num(info, "OLDPWD", buf);
+			getcwd(buf, 512);
+			chdir(ft_strjoin(tmp, info->line_tab[1]));
+			getcwd(buf, 512);
+			info = get_env_num(info, "PWD", buf);
+		}
 	}
 	dir == NULL ? 0 : closedir(dir);
 }
